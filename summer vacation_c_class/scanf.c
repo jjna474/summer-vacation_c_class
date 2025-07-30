d2c2d088bc065512eed27d5cf52ed877f009de1a
kimjina <kimjina0309@naver.com>
2025-07-30 16:45:43 +0900

    day5

---
+++ summer vacation_c_class/scanf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(void)
+{
+	//1.입력할 데이터의 자료형에 대응하는 형식문자
+	//2.입력할 데이터가 저장될 메모리 공간이 필요
+	//3.scanf->scanf_s
+	int num;
+	scanf_s("%d", &num);
+	printf("\n");
+
+	//scanf_s("입력:%d", &num);
+	printf("입력:");
+	scanf_s("%d", &num);
+	
+
+
+
+	return 0;
+}
---
+++ summer vacation_c_class/형식문자.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void)
+{
+	//%d:int,char,short(숫자로 출력)
+	//%c:char,short,int(문자로 출력)
+	//%f:float 실수
+	//%s:char(문자열)
+	//%p:포인터 주소값
+	//%lld:long long
+	//%u:unsigned  int(부호 없는 숫자)
+	//%lf:long double , double
+
+	long long x = 2200000000;
+	printf("값: %lld\n", x);
+
+	printf("%10d\n", 42);
+	printf("%5d\n", 12345);
+	printf("%5d\n", 123456);
+	printf("%-10d", 123456);
+	printf("%d", 1);
+	printf("%010d\n", 1234);
+	printf("%8.2f\n", 3.1415);//8:칸 개수  2:소수점 자리 수
+	printf("%8.3f", 3.1415);
+
+	return 0;
+}
